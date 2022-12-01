a1f0123b8fff0e0e3740e0e141102b0f25728ac3
Biagio Mamone <BMAM179@more2learn.de>
2022-12-01 10:24:43 +0100

    Neu

--- funktionen.js
+++ funktionen.js
@@ -2,43 +2,131 @@
 
 /* Primitives */
 
-// let a // (var) Definition oder Deklaration
+// let a; // (var) Definition oder Deklaration
 
 // Wertzuweisung / Assignment
+// a = 2; // number
+// a = true; // boolean (true, false)
+// a = "hallo"; // string
 
+// console.log(typeof a);
+// console.log(a);
 
 /* Arrays */
 // Navigation über INDEX
 
-
-
+// let arr;
+// // arr = new Array(); // Möglichkeit
+// arr = [false,true,true,false];
+// console.log(arr);
+// console.log(arr[0]);
+// console.log(arr[3]);
+// console.log(arr.lenght);
+// console.log(arr[arr.lenght-1]);
+// console.log(arr[5]);
 
 /* Array -Info, nicht klausurrelevant*/
-
+// let arr = [];
+// console.log(arr);
+// arr.push("a");
+// console.log(arr);
+// arr.push("b");
+// console.log(arr);
+// arr.pop();
+// console.log(arr);
+// arr.pop();
+// console.log(arr);
 
 /* Daten-Objekte */
 // Navigation über Bezeichner (keys)
+// let obj = {x:"Hi",y:20};
+// console.log(obj);
+// console.log(obj.x);
+// console.log(obj.y);
 
+// obj = 
+// {
+//     val0: 10,
+//     sub1:
+//     {
+//     val1: "hi",
+//     val2: 2
+//     }
+// };
 
+// console.log(obj);
+// console.log(obj.val0);
+// console.log(obj.sub1.val2);
+// console.log(obj.sub1.val1);
+// console.log(obj.sub1);
+// {{{{{{}}}}}} nested objekt
 
  /*DOM*/
+// DOM --> Modell der HTML-Struktur
+// DOM --> Document Objekt Model
+// DOM --> nachdem Parsen / Laden der HTML-Seite
 
+// console.log(document);
+// console.log(document.body);
+// console.log(document.body.children);
+// console.log(document.body.children[0]);
+// console.log(document.body.children[0].innerHTML);
+// document.body.children[0].innerText = "Hi";
+// console.log(document.getElementById("test"));
+// document.getElementById("test").innerHTML = "Hallo Text!"
 
 /* ENTSCHEIDUNGSSTRUKTUREN | CONTROL FLOW*/
 
+// let cond;
 
+// cond = true;
+// cond = (6 > 4);
+// cond = (6 < 4);
+// cond = (4 == 4); // Test auf Wert
+// cond = (4 === "4"); // Test auf Wert UND Typ
+
+// if (cond)
+// {
+//     console.log("Aussage wahr");
+// }
+// else
+// {
+//     console.log("Aussage falsch");
+// }
+
+// switch (key)
+// {
+//     case value:
+//         break;
+//     default:
+//         break;
+// }
 
  //   https://github.com/01-PP/03_Entscheidungsstrukturen
 
 /* FUNKTIONEN */
 
+// 1. Kapselung
+// test(); // call
+// function test() // Fkt.-Rumpf | body
+// { 
+//     console.log("Testausgabe");
+// }
+
+// 2. return
+//  console.log(test2());
+//  function test2() // do something ...
+//  { 
+//     return "Testausgabe 2"; // Lieferung an die Stelle des calls
+//     console.log("Noch eine Ausgabe"); // nach return Abbruch!
+//  }
 
 // 3. Parameter 
 //  console.log(test3("Robert")); // Argument(e)
 //  function test3(firstName) // Parameter
 //  { // do something ...
 //     return "Hallo, " + firstName; 
-// }
+//  }
 
 // 4. Fkt-Call aus Funktionen
 // console.log(test4("Robert")); // Argument(e)
